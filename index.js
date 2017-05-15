016c4f8d53c4eb7e6a05ca2d48d25a94306d0de9
Laurence Ruberl <bknd225@gmail.com>
2017-05-15 11:51:15 -0400

    add function to event listener

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ personForm.onsubmit = (ev) => {
     const details = document.querySelector('.details')
 
     const personName = form.personName.value
+    const hairColor = form.hairColor.value
+    const birthplace = form.birthplace.value
+    const age = form.age.value
 
-    // details.innerHTML = `<em>$[personName]</em>`
+    details.innerHTML = `<em> ${personName} </em>`
+    details.innerHTML = `
+            <ul>
+            <li>Name: ${personName}</li>
+            <li>Age: ${age}</li>
+            <li>Birthplace: ${birthplace}</li>
+            `
     
     const em = document.createElement('em')
     em.textContent = personName
