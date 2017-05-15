00e61cccc740046a0fd041e64cce8348c00670fe
Laurence Ruberl <bknd225@gmail.com>
2017-05-15 14:14:42 -0400

    Start using a for loop to append form fields

---
+++ READEME.md
@@ -0,0 +1,12 @@
+# day 1
+
+## homework
+
+Add form values to `.details` using `document.createElement` and `appendChild`, instead of `innerHTML`.
+
+### Bonus Credit
+
+Break out some of this functionality int a separate function
+
+### Super Mega Bonus Credit
+Do not hard-code the names of the fields in your JavaScript.  Loop over all of th elements in the form.
--- index.js
+++ index.js
@@ -5,24 +5,29 @@ const handleSubmit = (ev) => {
     const form = ev.target
     const details = document.querySelector('.details')
 
-    const personName = form.personName.value
-    const hairColor = form.hairColor.value
-    const birthplace = form.birthplace.value
-    const age = form.age.value
+    // const personName = form.personName.value
+    // const hairColor = form.hairColor.value
+    // const birthplace = form.birthplace.value
+    // const age = form.age.value
 
-    const colorDiv = `
-        <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>`
+    // const colorDiv = `
+    //     <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>`
     
 
-    details.innerHTML = `<em> ${personName} </em>`
-    details.innerHTML = `
-        <ul>
-            <li>Name: ${personName}</li>
-            <li>Hair Color: ${colorDiv}</li>
-            <li>Age: ${age}</li>
-            <li>Birthplace: ${birthplace}</li>
-        </u1>
-        `
+    // details.innerHTML = `<em> ${personName} </em>`
+    // details.innerHTML = `
+    //     <ul>
+    //         <li>Name: ${personName}</li>
+    //         <li>Hair Color: ${colorDiv}</li>
+    //         <li>Age: ${age}</li>
+    //         <li>Birthplace: ${birthplace}</li>
+    //     </u1>`
+        
+    for (var area in form){
+        const em = document.createElement("em")
+        em.textContent() = area.value
+        details.appendChild(em)
+    }
     }
 
 personForm.addEventListener('submit', handleSubmit)
