3fb31284fe711936701093c713117d1df0a7f067
Laurence Ruberl <bknd225@gmail.com>
2017-05-16 09:23:53 -0400

    Extract renderColor function

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const handleSubmit = (ev) => {
     ev.preventDefault()
     const form = ev.target
     const details = document.querySelector('.details')
-
     const labels = document.getElementsByTagName('label')
     const types = document.getElementsByTagName("input")
     const unList = document.createElement("ul")
@@ -13,10 +12,10 @@ const handleSubmit = (ev) => {
         if (types[i].type == 'color') {
             let colorDiv = document.createElement("div")
             colorDiv.style = `display=inLine-block;height: 50px; width: 100px; background-color: ${form[i].value}`
-           
+ 
             li.textContent = `${labels[i].innerText}:`
             li.appendChild(colorDiv)
-        } else {
+        } else if (types[i].type == "text" || types[i].type == "number"){
             li.textContent =labels[i].innerHTML + ": " + form[i].value
         }
         unList.appendChild(li)
